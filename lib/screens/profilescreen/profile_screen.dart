9f0aa8ac9d1a45837ed29f973fd0b872cc1added
fazeleardestani <fazeleardestani@gmail.com>
2023-09-03 18:15:08 +0330

    Remove extra dependencies

--- lib/screens/profilescreen/profile_screen.dart
+++ lib/screens/profilescreen/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:routine_app/res/colors.dart';
 import 'package:routine_app/res/texts.dart';
 import 'package:routine_app/shared/app_text.dart';
